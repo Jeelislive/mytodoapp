fd476b6ce8e9bd58922219345da2b40a932bf04d
JEEL RUPARELIYA <108856045+Jeelislive@users.noreply.github.com>
2024-06-02 19:51:12 +0530

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -20,7 +20,7 @@ const Login = () => {
                 },
                 body: JSON.stringify({ email, password })
             }
-            const response = await fetch('http://localhost:3000/login', option);
+            const response = await fetch('https://mytodoapp-fqj8.vercel.app/login', option);
             if (response.ok) {
                 navigate('/todo');
             } else {
